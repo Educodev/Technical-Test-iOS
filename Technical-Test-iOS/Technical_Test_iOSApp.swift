9b72ebc8cbd378f2c78426a747da251742603e14
Eduardo Herrera <edauardodesarrollador@icloud.com>
2023-02-20 15:54:43 +0100

    Initial Commit

---
+++ Technical-Test-iOS/Technical_Test_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Technical_Test_iOSApp.swift
+//  Technical-Test-iOS
+//
+//  Created by Eduardo Herrera on 20/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Technical_Test_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
